1f6e459d9646310fdea45a619a9c4f402eaa0c4e
Michelle Hanne Soares de Andrade <michellehanne.andrade@gmail.com>
2025-11-02 18:25:11 -0300

    Refactor philosopher dining problem implementation

--- Exemplos_Codigo/filosofo.c
+++ Exemplos_Codigo/filosofo.c
@@ -1,38 +1,100 @@
+/*
+O problema do Jantar dos filósofos, sem controle de impasses.
+
+Compilar com gcc -Wall filosofos.c -o filosofos -lpthread
+
+Carlos Maziero, DINF/UFPR 2020
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
 
+#define NUMFILO 5
 
-#define NUM_THREADS 16
+pthread_t filosofo [NUMFILO] ;	// threads filosofos
+sem_t     hashi    [NUMFILO] ;	// um semaforo para cada palito (iniciam em 1)
 
+// espaços para separar as colunas de impressão
+char *space[] = {"", "\t", "\t\t", "\t\t\t", "\t\t\t\t" } ;
 
-void *threadBody (void *id)
+// espera um tempo aleatório entre 0 e n segundos (float)
+void espera (int n)
 {
-	long tid =(long) id;
-	printf("t%2ld: Olá!\n", tid);
-	sleep(3);
-	printf("t%2ld: Tchau!!\n", tid);
-	
-	pthread_exit(NULL);
+  sleep (random() % n) ;		// pausa entre 0 e n segundos (inteiro)
+  usleep (random() % 1000000) ;	// pausa entre 0 e 1 segundo (float)
 }
 
-int main(int argc, char *argv[])
+// filósofo comendo
+void come (int f)
 {
-	pthread_t thread[NUM_THREADS];
-	long i, status ;
-	
-	for (i=0; i<N; i++)
-	{
-		printf("Main: criando thread %02ld\n", i);
-		
-		status= pthread_create (&thread[i], NULL, threadBody, (void *) i);
-		
-		if (status)
-		{
-			perror("phtread_create");
-			exit(1);
-		}
-	}
-	pthread_exit (NULL);
+  printf ("%sF%d COMENDO\n", space[f], f) ;
+  espera (1) ;
+}
+
+// filósofo meditando
+void medita (int f)
+{
+  printf ("%sF%d meditando\n", space[f], f) ;
+  espera (1) ;
+}
+
+// pega o hashi
+void pega_hashi (int f, int h)
+{
+  printf ("%sF%d quer  h%d\n", space[f], f, h) ;
+  sem_wait (&hashi [h]) ;
+  printf ("%sF%d pegou h%d\n", space[f], f, h) ;
+}
+
+// larga o hashi
+void larga_hashi (int f, int h)
+{
+  printf ("%sF%d larga h%d\n", space[f], f, h) ;
+  sem_post (&hashi [h]) ;
+}
+
+// corpo da thread filosofo
+void *threadFilosofo (void *arg)
+{
+  int i = (long int) arg ;
+  while (1)
+  {
+    medita (i) ;
+    pega_hashi (i, i) ;
+    pega_hashi (i, (i+1) % NUMFILO) ;
+    come (i) ;
+    larga_hashi (i, i) ;
+    larga_hashi (i, (i+1) % NUMFILO) ;
+  }
+  pthread_exit (NULL) ;
+}
+
+// programa principal
+int main (int argc, char *argv[])
+{
+  long i, status ;
+
+  // para o printf não se confundir com a threads
+  setvbuf (stdout, 0, _IONBF, 0) ;
+
+  // inicia os hashis
+  for(i=0; i<NUMFILO; i++)
+    sem_init (&hashi[i], 0, 1) ;
+
+  // inicia os filosofos
+  for(i=0; i<NUMFILO; i++)
+  {
+    status = pthread_create (&filosofo[i], NULL, threadFilosofo, (void *) i) ;
+    if (status)
+    {
+      perror ("pthread_create") ;
+      exit (1) ;
+    }
+  }
+
+  // a main encerra aqui
+  pthread_exit (NULL) ;
 }
